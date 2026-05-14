39a02f9b08b6e0aed41701df7deb9808fdee071d
bhagya <b.lokahnde1106@gmail.com>
2026-05-14 14:26:47 +0530

    modified

--- rearrange1.py
+++ rearrange1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re
+import sys
 
 def rearrange_name(name):
 
@@ -11,3 +12,4 @@ def rearrange_name(name):
         return name
 
     return "{} {}".format(result[2], result[1])
+sys.exit(0)
